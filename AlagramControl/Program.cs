9900481e2dad19d726691457c71f27d66ff6aea6
Samet <sametsenturkkk@outlook.com>
2018-12-08 17:20:04 +0300

    Add project files.

---
+++ AlagramControl/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlagramControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+
+            Console.Write("First Word => ");
+            a = Console.ReadLine();
+
+
+            Console.Write("Second Word => ");
+            b = Console.ReadLine();
+
+            Console.Clear();
+
+            bool isAnagram = false;
+
+            int needCount = 0;
+
+            if (a.Length != b.Length)
+                needCount += (a.Length - b.Length) < 0 ? (a.Length - b.Length) * -1 : (a.Length - b.Length) * 1;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                if (!b.Contains(a[i]) && !b.StartsWith(Convert.ToString(a[i])) && !b.EndsWith(Convert.ToString(a[i])))
+                {
+                    needCount++;
+                }
+                else
+                {
+                    b = b.Replace(a[i].ToString(), "");
+                }
+            }
+
+            if (needCount == 0)
+                isAnagram = true;
+
+
+            if (isAnagram)
+                Console.Write("These words already alagrams.");
+            else
+                Console.Write($"You need {needCount} word for anagram");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlagramControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AlagramControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlagramControl")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c49a1d00-2a73-4dd2-bdc3-4ad9b8216199")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
